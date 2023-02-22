5e393a379f4818fdb82757b96243555ba1791ade
EkuwaAdade <126028365+EkuwaAdade@users.noreply.github.com>
2023-02-22 20:36:22 +0000

    Update cachematrix.R
    cachematrixassignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## mkeCacheMatrix includes ser,get,setinv,getinv
+library(MASS)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+inv <- NULL    #this function initializes inverse as NULL
+set <- function(n){
+  m <<- n
+  inv <<- NULL
+}
+get <- function()m  #this function is for getting the matrix m
+setinv <- function(inverse)inv<<-inverse
+getinv <- function(){
+  inver <- ginv(m)
+  inver%%m      #this function obtains inverse of a matrix
+}
+list(set=set,get=get,
+     setinv = setinv,
+     getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## for cache data
+cacheSolve <- function(m, ...)##gets cached data
+  {
+  inv <- m$getinv()
+  if(!is.null(inv)){
+    message("getting cached data!")
+    return(inv)
+  }
+  data <- m4get()
+  inv <- solve(data,...)
+  m$setinv(inv)
+  inv
+        ## Return a matrix that is the inverse of 'm'
 }
